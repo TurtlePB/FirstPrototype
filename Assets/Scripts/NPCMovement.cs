2566038f3ca674f68d467f4d975a296a22e9ff40
TurtlePB <BreitensteinT@outlook.de>
2023-01-16 11:59:48 +0100

    NPC Behaviour & Sprite Randomizer

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -1,16 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+// using Random = System.Random;
+using Random = UnityEngine.Random;
 
 public class NPCMovement : MonoBehaviour
 {
+    // private BoxCollider2D bc;
     public float speed = 1f;
     public List<Transform> listOfWaypoints;
+    private SpriteRenderer sr;
+    public List<Sprite> listOfSprites;
+    // [SerializeField] private Transform Chair;
     private int currentIndexOfWaypoint;
-    public bool checkIfTableIsFree;
+    private int currentIndexOfSprites;
+    private bool isSitting;
+
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         currentIndexOfWaypoint = 0;
+        currentIndexOfSprites = 0;
+        isSitting = false;
     }
     
     void Update()
@@ -20,17 +33,37 @@ public class NPCMovement : MonoBehaviour
 
     void TableSearch()
     {
+        // int index = new Random().Next(listOfSprites.Count);
+        // print(index);
+        // gameObject.transform.
+        
+        if (sr)
+        {
+            sr.sprite = listOfSprites[Random.Range(0, listOfSprites.Count)];
+        }
+        
+        if (isSitting == true) return;
+        
         if (Vector3.Distance(transform.position, listOfWaypoints[currentIndexOfWaypoint].position) < 0.001f)
         {
             currentIndexOfWaypoint++;
-
+        
             if (currentIndexOfWaypoint > listOfWaypoints.Count)
             {
                 currentIndexOfWaypoint = 0;
             }
         }
 
-        var move = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, listOfWaypoints[currentIndexOfWaypoint].position, move);
+        if (transform.position != listOfWaypoints[2].position)
+        {
+            var move = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, listOfWaypoints[currentIndexOfWaypoint].position, move);
+        }
+        else
+        {
+            isSitting = true;
+        }
+        
     }
+    
 }
