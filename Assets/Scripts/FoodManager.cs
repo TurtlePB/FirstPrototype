ee19a93fcdd67c0328b8a38593af8ecbae1fee24
TurtlePB <BreitensteinT@outlook.de>
2023-01-17 12:22:03 +0100

    Selector attempt

--- Assets/Scripts/FoodManager.cs
+++ Assets/Scripts/FoodManager.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class FoodManager : MonoBehaviour
 {
-    // public List<object> food;
-    // private 
+    public int numSelectors = 5;
+    public GameObject[] selectorArr;
+    public GameObject selector; //selected in the editor
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        selectorArr = new GameObject[numSelectors];
+        for (int i = 0; i < numSelectors; i++)
+        {
+            GameObject go = Instantiate(selector, new Vector3((float)i, 1, 0), Quaternion.identity) as GameObject;
+            go.transform.localScale = Vector3.one;
+            selectorArr[i] = go;
+        }
     }
 }
